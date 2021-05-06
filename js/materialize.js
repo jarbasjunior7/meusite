4df7bc9f20b9510b6c14ecf83f5e250821cc0394
JarbasJrgit <jarbasn3249@gmail.com>
2021-05-06 17:18:09 -0300

    finalizado

--- js/materialize.js
+++ js/materialize.js
@@ -12390,3 +12390,12 @@ $(document).ready(function(){
   $('.modal').modal();
   $('.dropdown-trigger').dropdown();
 });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $('.dropdown-trigger').dropdown();
